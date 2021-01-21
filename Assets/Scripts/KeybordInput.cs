b066400e9efb84f87bf365a94cdd9a7cec96bfd6
Savidiy <savidiy@mail.ru>
2021-01-21 22:29:36 +0300

    Player Keyboard Input

---
+++ Assets/Scripts/KeybordInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeybordInput : MonoBehaviour
+{
+    [SerializeField] private float speed = 3;
+
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        //if (vertical != 0 || horizontal != 0)
+        //    Debug.Log($"vert = {vertical}, hoz = {horizontal}");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+        move.Normalize();
+
+        float dx = move.x * speed * Time.deltaTime;
+        float dy = move.y * speed * Time.deltaTime;
+
+        float newX = transform.position.x + dx;
+        float newY = transform.position.y + dy;
+        float newZ = transform.position.z;
+
+        Vector3 newPosition = new Vector3(newX, newY, newZ);
+
+        transform.position = newPosition;        
+    }
+}
