411ab11e840da97a73f31a971f458f538d981daa
Savidiy <savidiy@mail.ru>
2021-01-22 00:03:38 +0300

    enemy factory and enemy mofes left

---
+++ Assets/Scripts/EnemyFactory.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFactory : MonoBehaviour
+{
+    [SerializeField] float delay = 0.7f;
+    [SerializeField] GameObject enemyPrefab;
+    float timer = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= delay)
+        {
+            timer = 0;
+
+            GameObject enemy = Instantiate(enemyPrefab);
+
+            Camera camera = Camera.main;
+
+            var x = camera.orthographicSize * camera.aspect;
+            float minY = -camera.orthographicSize;
+            float maxY = camera.orthographicSize;
+            var y = Random.Range(minY, maxY);
+            Vector3 spawnPosition = new Vector3(x, y, 0);
+            enemy.transform.position = spawnPosition;
+
+            //Debug.Log($"Camera size ({camera.pixelWidth}|{camera.pixelHeight}). Aspect = {camera.aspect}");
+        }
+        
+    }
+}
---
+++ Assets/Scripts/SimpleEnemyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleEnemyController : MonoBehaviour
+{
+    [SerializeField] float dx = 3;
+    float leftScreenBorder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftScreenBorder = -1 * Camera.main.orthographicSize * Camera.main.aspect; // left border of screen
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(dx * Time.deltaTime, 0, 0);
+
+        if (transform.position.x < leftScreenBorder)
+            Destroy(gameObject);
+    }
+}
